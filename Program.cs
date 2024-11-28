09eb628c459927a9d5f5d472daf9261aa5a06fa8
Faik Karlihan <faik_han@outlook.com>
2024-11-28 12:03:25 +0300

    Final project

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Console application that prints true if there are 2 consonants side by side in the entered sentence or word, otherwise false.\n");
+
+Check chn = new Check();
+chn.CheckString();
+
+
